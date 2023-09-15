0b949c84329b3b4f2ee112dd5ef0c9ad4ea43061
panth1823 <panthshah242@gmail.com>
2023-09-15 22:52:33 +0530

    updated code

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="header">Your Personalized Todo List</div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      addTodo(value);
+      setValue("");
+    }
+  };
+  return (
+    
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="TodoInput"
+        value={value}
+        placeholder="Today's Task?"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <button type="submit" className="Todo-btn">
+        Add task
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
