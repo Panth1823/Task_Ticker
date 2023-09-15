563c2c1d3b1b6655b6dc7d9411913273c2b21e3f
panth1823 <panthshah242@gmail.com>
2023-09-15 22:35:56 +0530

    updated code

--- task_ticker/src/App.js
+++ task_ticker/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-
-import TodoWrapper from './components/TodoWrapper';
+import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
   return (
     <>
-    <Navbar/>
-      <div><TodoWrapper /></div>
-   
+      <Navbar />
+      <div>
+        <TodoWrapper />
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
--- task_ticker/src/components/TodoWrapper.js
+++ task_ticker/src/components/TodoWrapper.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import { Todo } from "./Todo";
-import {EditTodo} from './EditTodo';
+import { EditTodo } from './EditTodo';
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -27,20 +27,21 @@ export const TodoWrapper = () => {
   const editTodo = (id) => {
     setTodos(
       todos.map((todo) =>
+      
         todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
       )
     );
   };
   const editTask = (task, id) => {
-    setTodos(todos.map((todo) => todo.id === id?{...todo,task,isEditing:!todo.isEditing}:todo))
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo))
   }
   return (
-    <div className="TodoWrapper bg-red-600">
+    <div className="TodoWrapper ">
       <h1 >Task Ticker</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo) =>
         todo.isEditing ? (
-          <EditTodo editTodo={editTask} task={todo}/>
+          <EditTodo key={todo.id} editTodo={editTask} task={todo} />
         ) : (
           <Todo
             task={todo}
