b9d742b0fa5f30b85f88d81882d9cd7040d58c87
panth1823 <panthshah242@gmail.com>
2023-11-30 11:54:38 +0530

    new priority dropdow added

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
-export const EditTodo = ({ editTodo,task }) => {
+export const EditTodo = ({ editTodo, task }) => {
   const [value, setValue] = useState(task.task);
+  const [priority, setPriority] = useState('low');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    editTodo(value,task.id);
-    
-   
+    editTodo(value, task.id, priority);
+
+
   };
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
@@ -23,8 +24,19 @@ export const EditTodo = ({ editTodo,task }) => {
       <button type="submit" className="Todo-btn">
         Add Task
       </button>
+
+      <select
+        className="dropdown"
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+      >
+        <option value="low" className="low">Low</option>
+        <option value="medium" className="medium">Medium</option>
+        <option value="high" className="high">High</option>
+      </select>
+      <button type="submit">Submit</button>
     </form>
   );
 };
 
-export default  EditTodo;
+export default EditTodo;
